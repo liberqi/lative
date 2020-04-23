009e7bcaebecabb1121174ece190c954fa17bb96
liberqi <zhangqi0525@outlook.com>
2020-04-23 16:48:44 +0800

    Update spot.js

--- spot.js
+++ spot.js
@@ -12,11 +12,11 @@
         document.getElementById('go').removeEventListener('click', start);
         promise.then(() => engine.toText('L'))
         .then(() => engine.shake())
-        .then(() => engine.toText('O'))
+        .then(() => engine.toText('唐'))
         .then(() => engine.shake())
-        .then(() => engine.toText('V'))
+        .then(() => engine.toText('章'))
         .then(() => engine.shake())
-        .then(() => engine.toText('E'))
+        .then(() => engine.toText('梅'))
         .then(() => engine.shake())
         .then(() => engine.clear())
         .then(() => document.getElementById('go').addEventListener('click', start));
